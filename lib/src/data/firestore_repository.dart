e3b8b8bb6050907c5063da4c2951ec33f6eab956
Nino Gjoni <49320058+ninogjoni@users.noreply.github.com>
2023-05-01 03:13:53 +0200

    firebase_basics_lesson19

---
+++ lib/src/data/firestore_repository.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faker_app_flutter_firebase/src/data/job.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FirestoreRepository {
+  FirestoreRepository(this._firestore);
+  final FirebaseFirestore _firestore;
+
+  Future<void> addJob(String uid, String title, String company) =>
+      _firestore.collection('jobs').add({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      });
+
+  /// access the .doc by adding the $jobId to the "path"
+  Future<void> updateJob(
+          String uid, String jobId, String title, String company) =>
+
+      /// OR _firestore.collection('jobs').doc(jobId).update({ ... })
+      _firestore.doc('jobs/$jobId').update({
+        'uid': uid,
+        'title': title,
+        'company': company,
+      });
+
+  Future<void> deleteJob(String uid, String jobId) =>
+
+      /// OR       _firestore.collection('jobs').doc(jobId).delete();
+      _firestore.doc('jobs/$jobId').delete();
+
+  Query<Job> jobsQuery() {
+    return _firestore.collection('jobs').withConverter(
+          fromFirestore: (snapshot, _) => Job.fromMap(snapshot.data()!),
+          toFirestore: (job, _) => job.toMap(),
+        );
+  }
+}
+
+final firestoreRepositoryProvider = Provider<FirestoreRepository>((ref) {
+  return FirestoreRepository(FirebaseFirestore.instance);
+});
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faker/faker.dart' hide Job;
+import 'package:faker_app_flutter_firebase/src/data/firestore_repository.dart';
+import 'package:faker_app_flutter_firebase/src/data/job.dart';
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -15,12 +20,63 @@ class HomeScreen extends ConsumerWidget {
           onPressed: () => context.goNamed(AppRoute.profile.name),
         )
       ]),
+      body: const JobsListView(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          debugPrint('Not implemented');
+          final user = ref.read(firebaseAuthProvider).currentUser;
+          final faker = Faker();
+          final title = faker.job.title();
+          final company = faker.company.name();
+          ref.read(firestoreRepositoryProvider).addJob(
+                user!.uid,
+                title,
+                company,
+              );
         },
       ),
+    );
+  }
+}
+
+class JobsListView extends ConsumerWidget {
+  const JobsListView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firestoreRepository = ref.watch(firestoreRepositoryProvider);
+    return FirestoreListView<Job>(
+      query: firestoreRepository.jobsQuery(),
+      // Automatically update the list when sth. in collection changes
+      itemBuilder: (BuildContext context, QueryDocumentSnapshot<Job> doc) {
+        final job = doc.data();
+        return Dismissible(
+          key: Key(doc.id),
+          background: const ColoredBox(color: Colors.red),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            final user = ref.read(firebaseAuthProvider).currentUser;
+            ref.read(firestoreRepositoryProvider).deleteJob(user!.uid, doc.id);
+          },
+          child: ListTile(
+            title: Text(job.title),
+            subtitle: Text(job.company),
+            onTap: () {
+              final user = ref.read(firebaseAuthProvider).currentUser;
+              final faker = Faker();
+              final title = faker.job.title();
+              final company = faker.company.name();
+              ref.read(firestoreRepositoryProvider).updateJob(
+                    user!.uid,
+                    // ID of the document (not defined in Job class)
+                    doc.id,
+                    title,
+                    company,
+                  );
+            },
+          ),
+        );
+      },
     );
   }
 }
