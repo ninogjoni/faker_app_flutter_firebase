b9fb933a6370df523f8b3b02d250280eb29b8088
Nino Gjoni <49320058+ninogjoni@users.noreply.github.com>
2023-05-01 02:02:09 +0200

    firebase_basics_lesson9

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,5 +1,7 @@
+import 'package:faker_app_flutter_firebase/src/routing/go_router_refresh_stream.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_profile_screen.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -8,15 +10,34 @@ enum AppRoute {
   profile,
 }
 
+final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+  return FirebaseAuth.instance;
+});
+
 final goRouterProvider = Provider<GoRouter>((ref) {
+  final firebaseAuth = ref.watch(firebaseAuthProvider);
   return GoRouter(
     initialLocation: '/sign-in',
     debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final isLoggedIn = firebaseAuth.currentUser != null;
+      if (isLoggedIn) {
+        if (state.location == '/sign-in') {
+          return '/profile';
+        }
+      } else {
+        if (state.location == '/profile') {
+          return '/sign-in';
+        }
+      }
+      return null;
+    },
+    refreshListenable: GoRouterRefreshStream(firebaseAuth.authStateChanges()),
     routes: [
       GoRoute(
         path: '/sign-in',
         name: AppRoute.signIn.name,
-        builder: (context, state) => const CustomSignInScreen(),
+        builder: (context, state) => CustomSignInScreen(),
       ),
       GoRoute(
         path: '/profile',
